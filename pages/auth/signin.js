efa30678ee73f5db24533f4409651261b0a46a97
carlosTavares1 <cast.3@hotmail.com>
2022-10-12 00:27:49 -0300

    authentication using google

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,9 +1,28 @@
-function signIn() {
+import { async } from "@firebase/util"
+import { getProviders, signIn as SignIntoProvider } from "next-auth/react"
+
+function signIn({ providers }) {
     return (
-        <div>
-            <h1>I am a SignIn Page</h1>
-        </div>
+        <>
+            {Object.values(providers).map((provider) => (
+                <div key={provider.name}>
+                    <button onClick={() => SignIntoProvider(provider.id)}>
+                        Sign in with {provider.name}
+                    </button>
+                </div>
+            ))}
+        </>
     )
+}
+
+export async function getServerSideProps(){
+    const providers = await getProviders();
+
+    return{
+        props:{
+            providers
+        }
+    }
 }
 
 export default signIn
